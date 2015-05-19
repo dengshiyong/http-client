be20ca074faffd6db31cf7cdcf529848d4daddc3
sfod <octosyllabic@gmail.com>
2015-05-19 12:30:30 +0300

    Set all request parameters in init function

--- src/http_client.c
+++ src/http_client.c
@@ -1,15 +1,30 @@
 #include "http_client.h"
-#include <curl/curl.h>
 
-int http_client_make_get_request(const char *url)
+int http_client_init(http_client_t *http_client, const char *url, write_cb_t *write_cb, void *userp)
 {
     if (!url) {
         return -1;
     }
-    CURL *easy_handle = curl_easy_init();
-    curl_easy_setopt(easy_handle, CURLOPT_URL, url);
-    curl_easy_setopt(easy_handle, CURLOPT_VERBOSE, 1);
-    curl_easy_perform(easy_handle);
-    curl_easy_cleanup(easy_handle);
+
+    http_client->handle = curl_easy_init();
+    if (!http_client->handle) {
+        return -1;
+    }
+    curl_easy_setopt(http_client->handle, CURLOPT_URL, url);
+    curl_easy_setopt(http_client->handle, CURLOPT_WRITEFUNCTION, write_cb);
+    if (userp) {
+        curl_easy_setopt(http_client->handle, CURLOPT_WRITEDATA, userp);
+    }
+    return 0;
+}
+
+void http_client_cleanup(http_client_t *http_client)
+{
+    curl_easy_cleanup(http_client->handle);
+}
+
+int http_client_make_get_request(const http_client_t *http_client)
+{
+    curl_easy_perform(http_client->handle);
     return 0;
 }
